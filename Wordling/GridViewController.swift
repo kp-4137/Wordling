508937f20c363b2387e2a9701be874fe53802a7c
Kishan <krp007kishanrpatel@gmail.com>
2023-05-02 02:53:45 -0700

    Created grid layout by reusing KeyCell in GridVC

--- Wordling/GridViewController.swift
+++ Wordling/GridViewController.swift
@@ -4,21 +4,25 @@
 //
 //  Created by Kishan Patel on 4/27/23.
 //
+
 import UIKit
 
-class GridViewController: UIViewController {
-
+class GridViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+    
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumInteritemSpacing = 4
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .blue
+        collectionView.backgroundColor = .clear
+        collectionView.register(KeyCell.self, forCellWithReuseIdentifier: KeyCell.identifier)
         return collectionView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
         view.addSubview(collectionView)
         NSLayoutConstraint.activate([
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
@@ -28,14 +32,41 @@ class GridViewController: UIViewController {
         ])
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension GridViewController {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 6
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView
+                    .dequeueReusableCell(withReuseIdentifier: KeyCell.identifier, for: indexPath) as? KeyCell
+                    else{
+                        fatalError()
+                    }
+        cell.backgroundColor = nil
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.systemGray3.cgColor
+        cell.layer.cornerRadius = 5
+        cell.label.textColor = .systemGray
+        cell.label.font = UIFont.boldSystemFont(ofSize: 20)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 20
+        let size: CGFloat = (collectionView.frame.size.width - margin) / 5
+        
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+    }
 
 }
